28643d22496ab1a86b1b62f8576fc24a9ceaa036
lmk15391997 <598812989@qq.com>
2019-03-06 22:03:39 +0800

    添加了深职知多少2

---
+++ szzds2/js/custom.js
@@ -0,0 +1,21 @@
+function goBack(){
+    window.history.back();
+}
+function goNotifications(){
+    window.location.assign("notifications.html");
+}
+function goForum(){
+    window.location.assign("forum.html");
+}
+function goKnow(){
+    window.location.assign("know.html");
+}
+function goHome(){
+    window.location.assign("首页.html");
+}
+function goEdit(){
+    window.location.assign("edit.html");
+}
+function goContent(){
+    window.location.assign("know_content.html");
+}
